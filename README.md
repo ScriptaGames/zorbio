f83be4b5beb98004d3880de7fe8aa53f52e289f6
Jared Sprague <jsprague@redhat.com>
2015-10-06 16:58:35 -0400

    Refactored the project into client/ server/ model, got basic socket connections working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Hug me
 A game where hugging helps you grow!
+
+## Install
+1. bower install
+2. npm install
+
+## Run
+npm start
--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,78 @@
-// Get the canvas element from our HTML above
-var canvas = document.getElementById("renderCanvas");
+var playerName;
+var playerNameInput = document.getElementById('playerNameInput');
+var socket;
+
+var canvas = document.getElementById('renderCanvas');
 
 // Load the BABYLON 3D engine
 var engine = new BABYLON.Engine(canvas, true);
 
 var MOVE_SPEED_SCALE = 0.08;
+
+var KEY_ENTER = 13;
+
+var game = new Game();
+
+function startGame() {
+    playerName = playerNameInput.value.replace(/(<([^>]+)>)/ig, '');
+    document.getElementById('gameAreaWrapper').style.display = 'block';
+    document.getElementById('startMenuWrapper').style.display = 'none';
+    socket = io();
+    SetupSocket(socket);
+
+    canvas = document.getElementById('renderCanvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // create the scene
+    var scene = createScene();
+
+    // Register a render loop to repeatedly render the scene
+    engine.runRenderLoop(function () {
+        scene.render();
+        gameLoop();
+    });
+}
+
+// check if nick is valid alphanumeric characters (and underscores)
+function validNick() {
+    var regex = /^\w*$/;
+    console.log('Regex Test', regex.exec(playerNameInput.value));
+    return regex.exec(playerNameInput.value) !== null;
+}
+
+window.onload = function() {
+    'use strict';
+
+    var btn = document.getElementById('startButton'),
+        nickErrorText = document.querySelector('#startMenu .input-error');
+
+    btn.onclick = function () {
+
+        // check if the nick is valid
+        if (validNick()) {
+            startGame();
+        } else {
+            nickErrorText.style.display = 'inline';
+        }
+    };
+
+    playerNameInput.addEventListener('keypress', function (e) {
+        var key = e.which || e.keyCode;
+
+        if (key === KEY_ENTER) {
+            if (validNick()) {
+                startGame();
+            } else {
+                nickErrorText.style.display = 'inline';
+            }
+        }
+    });
+};
+
+function SetupSocket(socket) {
+  game.handleNetwork(socket);
+}
 
 // This begins the creation of a function that we will 'call' just after it's built
 var createScene = function () {
@@ -88,7 +156,7 @@ var createScene = function () {
     camera.speed = 5;
     camera.angularSensibility = 200;
 
-     //This attaches the camera to the canvas
+    //This attaches the camera to the canvas
     camera.attachControl(canvas, false);
 
     //var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
@@ -124,13 +192,10 @@ var createScene = function () {
 
 };  // End of createScene function
 
-// Now, call the createScene function that you just finished creating
-var scene = createScene();
-
-// Register a render loop to repeatedly render the scene
-engine.runRenderLoop(function () {
-    scene.render();
-});
+function gameLoop() {
+    game.handleLogic();
+    game.handleGraphics(canvas);
+}
 
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
