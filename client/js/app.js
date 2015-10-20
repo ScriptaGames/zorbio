576cca466cf51d9376446ec222e97455df0b5457
mwcz <mwc@clayto.com>
2015-10-19 21:29:09 -0400

    sphere sticks to camera when panning

--- client/js/app.js
+++ client/js/app.js
@@ -106,6 +106,8 @@ function createScene() {
         renderer.setPixelRatio( window.devicePixelRatio );
         renderer.setSize( window.innerWidth, window.innerHeight );
 
+        // orbit camera
+
         camera = new THREE.PerspectiveCamera(
             65,
             window.innerWidth / window.innerHeight,
@@ -114,11 +116,14 @@ function createScene() {
         );
         camera.position.z = 200;
 
-        controls = new THREE.OrbitControls( camera, renderer.domElement );
-        //controls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
+        controls = new THREE.FollowOrbitControls( camera, renderer.domElement );
         controls.enableDamping = true;
         controls.dampingFactor = 0.25;
         controls.enableZoom = true;
+        // controls.minDistance = 30;
+        // controls.maxDistance = 30;
+        // controls.minPolarAngle = 0; // radians
+        // controls.maxPolarAngle = Math.PI*2; // radians
 
         // sphere
 
@@ -132,6 +137,7 @@ function createScene() {
         // sphere.renderOrder = -1;
         scene.add( sphere );
 
+        controls.target = sphere;
         // food
 
         drawFood();
@@ -172,118 +178,29 @@ function createScene() {
     function animate() {
 
         requestAnimationFrame( animate );
+
         controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
+        // controls.target.set( sphere.position );
+
         render();
 
     }
 
     function render() {
 
-        // TODO: remove this, it's just a demo of toggling food particles
-        var time = (new Date()).getTime();
-        var fi = time % food.living.length;
-        hideFood(fi);
-        showFood( ( fi + food.living.length / 2 ) % food.living.length );
-        // ENDTODO: remove this, it's just a demo of toggling food particles
+        // // TODO: remove this, it's just a demo of toggling food particles
+        // var time = (new Date()).getTime();
+        // var fi = time % food.living.length;
+        // hideFood(fi);
+        // showFood( ( fi + food.living.length / 2 ) % food.living.length );
+        // // ENDTODO: remove this, it's just a demo of toggling food particles
+
+        handleKeysDown();
 
         renderer.render( scene, camera );
 
     }
 }
-
-// This begins the creation of a function that we will 'call' just after it's built
-//function createScene() {
-
-//    // Now create a basic Babylon Scene object
-//    scene = new THREE.Scene();
-
-//    scene = new THREE.Scene();
-//    // scene.fog = new THREE.FogExp2( 0xffffff, 0.002 );
-
-//    renderer = new THREE.WebGLRenderer({ canvas: canvas });
-//    renderer.setClearColor( THREE.ColorKeywords.white );
-//    renderer.setPixelRatio( window.devicePixelRatio );
-//    renderer.setSize( screenWidth, screenHeight );
-
-//    // var sphereRef = drawPlayerSphere(player.sphere);
-//    var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-//    var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-//    sphere = new THREE.Mesh( geometry, material );
-//    scene.add( sphere );
-
-//    // This creates and positions a camera
-//    camera = new THREE.PerspectiveCamera( 60, screenWidth, screenHeight, 1, 1000);
-//    camera.position.z = 500;
-//    camera.lookAt(sphere);
-//    // controls = new THREE.OrbitControls( camera, renderer.domElement);
-//    // camera.inertia = 0.01;
-//    // camera.target = sphereRef;
-//    // camera.lowerRadiusLimit = 4;
-//    // camera.upperRadiusLimit = 150;
-//    // camera.speed = 5;
-//    // camera.angularSensibility = 200;
-
-//    //This attaches the camera to the canvas
-//    // camera.attachControl(canvas, false);
-
-//    //var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
-//    //camera.setPosition(new BABYLON.Vector3(-15, 3, 0));
-
-//    // Skybox
-//    // var skybox = BABYLON.Mesh.CreateBox("skyBox", zorbioModel.worldSize.x, scene);
-//    // var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-//    // skyboxMaterial.backFaceCulling = false;
-//    // skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox_grid", scene);
-//    // skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-//    // skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-//    // skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-//    // skybox.material = skyboxMaterial;
-
-//    // Draw other actors currently in the game to the scene
-//    drawActors();
-//    drawFood(zorbioModel, scene);
-
-//    // Save a reference to the sphere created for the player
-//    // player.sphere.geo = sphereRef;
-//    // zorbioModel.actors[player.sphere.id] = player.sphere;
-
-//    //scene.registerBeforeRender(function updateSpherePosition() {
-//    //    var sphereGeo = player.sphere.geo;
-
-//    //    // move forward in the direction the camera is facing
-//    //    var move_speed = MOVE_SPEED_SCALE * (player.sphere.throttle / 100);
-//    //    var camera_angle_vector = camera.position.subtract(sphereGeo.position).normalize();
-//    //    camera_angle_vector.multiplyInPlace(new BABYLON.Vector3(move_speed, move_speed, move_speed));
-//    //    sphereGeo.position.subtractInPlace(camera_angle_vector);
-
-//    //    //TODO: talk to MC about this not sure if I have to re-assign the geo after changing it
-//    //    player.sphere.geo = sphereGeo;
-//    //    player.sphere.position = sphereGeo.position;
-//    //    zorbioModel.actors[player.sphere.id] = player.sphere;
-
-//    //    updateActors();
-//    //});
-
-//    //scene.registerBeforeRender(function() {
-//    //    camera.alpha += 0.01 * scene.getAnimationRatio();
-//    //});
-
-//    // Leave this function
-
-//    function animate() {
-//        requestAnimationFrame( animate );
-//        // controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
-//        render();
-//    }
-
-//    function render() {
-//        renderer.render(scene, camera);
-//    }
-
-//    animate();
-
-//    return scene;
-//}
 
 function drawActors() {
     var actors = zorbioModel.actors;
@@ -407,42 +324,6 @@ function drawFood() {
     food.particleSystem = new THREE.Points( geometry, material );
     scene.add( food.particleSystem );
 
-    //var foodTexture = THREE.ImageUtils.loadTexture( 'textures/solid-particle.png' );
-    //var material = new THREE.SpriteMaterial({ map: foodTexture, useScreenCoordinates: false});
-    ////var material = new THREE.SpriteMaterial();
-    //material.depthTest = true;
-    //material.transparent = false;
-    //material.opacity = 1.0;
-
-    //var particle;
-    //var size = 6;
-    //var offset = 0;
-    //for (var i = 0; i < zorbioModel.foodCount; i++) {
-
-    //    var X = zorbioModel.food[offset];
-    //    var Y = zorbioModel.food[offset + 1];
-    //    var Z = zorbioModel.food[offset + 2];
-    //    var R = zorbioModel.food[offset + 3];
-    //    var G = zorbioModel.food[offset + 4];
-    //    var B = zorbioModel.food[offset + 5];
-
-    //    material.color = new THREE.Color(R/255, G/255, B/255);
-
-    //    material.needsUpdate = true;
-
-    //    // material.color = new THREE.Color("rgb(" + colorStyle + ")");
-
-    //    particle = new THREE.Sprite( material );
-    //    particle.scale.x = particle.scale.y = 10;
-    //    particle.position.x = X;
-    //    particle.position.y = Y;
-    //    particle.position.z = Z;
-
-    //    scene.add( particle );
-
-    //    offset += size;
-    //}
-
 }
 
 window.addEventListener("keydown", handleKeydown);
@@ -451,8 +332,8 @@ window.addEventListener("keyup", handleKeyup);
 var KeysDown = {};
 var KeyCodes = {
     87 : 'w',
-    83 : 'a',
-    65 : 's',
+    83 : 's',
+    65 : 'a',
     68 : 'd',
     32 : 'space',
     16 : 'shift',
@@ -469,7 +350,7 @@ function handleKeydown(evt) {
     if (we_care_about_this_key) {
         already_pressed = KeysDown[KeyCodes[evt.keyCode]];
         if (!already_pressed) {
-            keyPressed(KeyCodes[evt.keyCode]);
+            keyJustPressed(KeyCodes[evt.keyCode]);
         }
         KeysDown[KeyCodes[evt.keyCode]] = true;
     }
@@ -484,7 +365,29 @@ function handleKeyup(evt) {
         KeysDown[KeyCodes[evt.keyCode]] = false;
     }
 }
-function keyPressed(key) {
+function handleKeysDown() {
+    for( var key in KeysDown ) {
+        if (KeysDown[key]) {
+            keyDown(key);
+        }
+    }
+}
+var keyDownMoveVector;
+function keyDown(key) {
+    keyDownMoveVector = sphere.position.clone();
+    if (key === 'w') {
+        keyDownMoveVector.sub(camera.position);
+        keyDownMoveVector.multiplyScalar(-1);
+        keyDownMoveVector.normalize();
+        sphere.position.sub(keyDownMoveVector);
+    }
+    else if (key === 's') {
+        keyDownMoveVector.sub(camera.position);
+        keyDownMoveVector.normalize();
+        sphere.position.sub(keyDownMoveVector);
+    }
+}
+function keyJustPressed(key) {
     console.log('key ' + key + ' just pressed');
 }
 function keyReleased(key) {
