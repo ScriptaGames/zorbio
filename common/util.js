d9b3ee46b288a1fa5e9048ad963578dbdf0c901e
mwcz <mwc@clayto.com>
2015-12-23 11:58:11 -0500

    update jsdoc for utils

--- common/util.js
+++ common/util.js
@@ -2,20 +2,31 @@ var NODEJS = typeof module !== 'undefined' && module.exports;
 
 var UTIL = {};
 
-// Returns a random integer between min (included) and max (included)
-// Using Math.round() will give you a non-uniform distribution!
+/**
+ * Returns a random integer between min (included) and max (included) Using
+ * Math.round() will give you a non-uniform distribution!
+ * @param {Number} min the minimum bound (inclusive)
+ * @param {Number} max the maximum bound (inclusive)
+ * @returns {Number} a random integer between min (included) and max (included)
+ */
 UTIL.getRandomIntInclusive = function UTILGetRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// Returns a random number between min (inclusive) and max (exclusive)
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ * @param {Number} min the minimum bound (inclusive)
+ * @param {Number} max the maximum bound (exclusive)
+ * @returns {Number} a random integer between min (included) and max (included)
+ */
 UTIL.getRandomArbitrary = function UTILGetRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 };
 
 /**
  * Checks for a valid nick
- * @param nickname
+ * @param {String} nickname
  * @returns {boolean}
  */
 UTIL.validNick = function UTILValidNick (nickname) {
@@ -25,11 +36,11 @@ UTIL.validNick = function UTILValidNick (nickname) {
 
 /**
  * Detect hitting the wall in the positive direction
- * @param p
- * @param r
- * @param v
- * @param w
- * @param axis
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
+ * @param {'x'|'y'|'z'} axis the axis on which to test for collision
  * @returns {boolean}
  */
 UTIL.hitp = function UTILhitp( p, r, v, w, axis ) {
@@ -38,11 +49,11 @@ UTIL.hitp = function UTILhitp( p, r, v, w, axis ) {
 
 /**
  * Detect hitting the wall in the negative direction
- * @param p
- * @param r
- * @param v
- * @param w
- * @param axis
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
+ * @param {'x'|'y'|'z'} axis the axis on which to test for collision
  * @returns {boolean}
  */
 UTIL.hitn = function UTILhitn( p, r, v, w, axis ) {
@@ -51,10 +62,10 @@ UTIL.hitn = function UTILhitn( p, r, v, w, axis ) {
 
 /**
  * Detect hit on the x axis in positive direction.
- * @param p
- * @param r
- * @param v
- * @param w
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
  * @returns {boolean}
  */
 UTIL.hitxp = function UTILhitxp( p, r, v, w ) {
@@ -64,10 +75,10 @@ UTIL.hitxp = function UTILhitxp( p, r, v, w ) {
 
 /**
  * Detect hit on the x axis in the negative direction.
- * @param p
- * @param r
- * @param v
- * @param w
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
  * @returns {boolean}
  */
 UTIL.hitxn = function UTILhitxn( p, r, v, w ) {
@@ -76,10 +87,10 @@ UTIL.hitxn = function UTILhitxn( p, r, v, w ) {
 
 /**
  * Detect hit on the y axis in the positive direction.
- * @param p
- * @param r
- * @param v
- * @param w
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
  * @returns {boolean}
  */
 UTIL.hityp = function UTILhityp( p, r, v, w ) {
@@ -88,10 +99,10 @@ UTIL.hityp = function UTILhityp( p, r, v, w ) {
 
 /**
  * Detect hit on the y axis in the negative direction.
- * @param p
- * @param r
- * @param v
- * @param w
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
  * @returns {boolean}
  */
 UTIL.hityn = function UTILhityn( p, r, v, w ) {
@@ -100,10 +111,10 @@ UTIL.hityn = function UTILhityn( p, r, v, w ) {
 
 /**
  * Detect hit on the z axis in the positive direction.
- * @param p
- * @param r
- * @param v
- * @param w
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
  * @returns {boolean}
  */
 UTIL.hitzp = function UTILhitzp( p, r, v, w ) {
@@ -112,10 +123,10 @@ UTIL.hitzp = function UTILhitzp( p, r, v, w ) {
 
 /**
  * Detect hit on the z axis in the netative direction.
- * @param p
- * @param r
- * @param v
- * @param w
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
  * @returns {boolean}
  */
 UTIL.hitzn = function UTILhitzn( p, r, v, w ) {
@@ -124,16 +135,14 @@ UTIL.hitzn = function UTILhitzn( p, r, v, w ) {
 
 
 /**
- * Returns true if a player is touching a wall
- * @param p
- * @param r
- * @param v
- * @param w
- * @returns {boolean}
+ * Returns true if a sphere would intersect a wall after applying velocity (v).
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
+ * @returns {boolean} whether the wall would be hit after applying given velocity
  */
 UTIL.checkWallCollision = function UTILcheckWallCollision( p, r, v, w ) {
-
-    // TODO: make sure when a collision occurs with two or more walls at once happens, it is handled correctly
 
     return UTIL.hitxp( p, r, v, w ) ||
         UTIL.hitxn( p, r, v, w ) ||
@@ -145,15 +154,20 @@ UTIL.checkWallCollision = function UTILcheckWallCollision( p, r, v, w ) {
 };
 
 /**
- * Adjust velocity on an object that has hit a wall based on axis
- * @param p
- * @param r
- * @param v
- * @param w
- * @returns {*}
+ * Adjust a sphere's velocity to prevent it from hitting a wall.  If no wall would be hit, velocity is not changed.
+ * @param {Vector3} p the position of the sphere being tested
+ * @param {Number} r the radius of the sphere being tested
+ * @param {Vector3} v the velocity of the sphere being tested
+ * @param {Vector3} w the world dimensions
+ * @returns {Vector3} the updated (or not) velocity
  */
 UTIL.adjustVelocityWallHit = function UTILadjustVelocityWallHit( p, r, v, w ) {
     var vs = v.clone();
+
+    // TODO: instead of reducing velocities to 0, reduce them just enough to
+    // avoid collision.  reducing to 0 causes small, fast spheres to sometimes
+    // "hit" the wall before they are visibly touching it.
+
     if ( UTIL.hitxp( p, r, v, w ) || UTIL.hitxn( p, r, v, w ) )
         vs.x = 0;
 
