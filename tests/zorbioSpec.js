9374cdbb81b4421469786c108c5ab7803ea259aa
mwcz <mwc@clayto.com>
2015-12-25 23:06:33 -0500

    avoid global in zorbio model test, add .players test

--- tests/zorbioSpec.js
+++ tests/zorbioSpec.js
@@ -1,17 +1,23 @@
 describe('ZOR.Model', function () {
     it('should be constructable', function () {
-        model = new ZOR.Model();
+        var model = new ZOR.Model( 0, 0 );
         expect(model instanceof ZOR.Model).toBe(true);
     });
 
     it('should have no actors at first', function () {
+        var model = new ZOR.Model( 0, 0 );
         expect(Object.getOwnPropertyNames(model.actors).length).toEqual(0);
+    });
+
+    it('should have no players at first', function () {
+        var model = new ZOR.Model( 0, 0 );
+        expect(Object.getOwnPropertyNames(model.players).length).toEqual(0);
     });
 
     it('should accept worldsize and food density', function () {
         var size = 200;
-        var food = 200;
-        model = new ZOR.Model(size, food);
+        var food = 10;
+        var model = new ZOR.Model( size, food );
         expect(model.worldSize.x).toEqual(size);
         expect(model.worldSize.y).toEqual(size);
         expect(model.worldSize.z).toEqual(size);
