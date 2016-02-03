8ed4f883183b2361c89b316b84502f5cd717a232
mwcz <mwc@clayto.com>
2016-02-03 18:52:13 -0500

    include favicon in build, fixes #118

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p dist > /dev/null
 cp -r client/textures dist/
+cp client/favicon.ico dist/
 node node_modules/html-inline/bin/cmd.js -i client/index.html > dist/index-inlined.html
 node node_modules/html-minifier/cli.js dist/index-inlined.html --minify-js --collapse-whitespace --remove-comments  --remove-attribute-quotes --remove-script-type-attributes --remove-style-link-type-attributes > dist/index.html
 rm dist/index-inlined.html
