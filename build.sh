24d459a5939c070ef2808beb67827a526fd35bfe
Jared Sprague <jared@caramelcode.com>
2016-02-03 21:03:55 -0500

    Finished RPM packaging! #106

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ node node_modules/html-inline/bin/cmd.js -i client/index.html > dist/index-inlin
 node node_modules/html-minifier/cli.js dist/index-inlined.html --minify-js --collapse-whitespace --remove-comments  --remove-attribute-quotes --remove-script-type-attributes --remove-style-link-type-attributes > dist/index.html
 rm dist/index-inlined.html
 echo "dist/index.html written"
+
+# generate the RPM spec file
+VERSION="$(util/jsawk 'return this.version' < package.json)"
+BUILD=$1
+rm -f zorbio.spec
+cp zorbio.spec.template zorbio.spec
+sed -i "s/VERSION/$VERSION/g" zorbio.spec
+sed -i "s/BUILD/$BUILD/g" zorbio.spec
