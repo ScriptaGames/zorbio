c50fa79cde60b8ffb999766f4b40726a9d05ea72
Jared Sprague <jared@caramelcode.com>
2016-02-10 20:50:02 -0500

    increased food tolerance #94

--- common/config.js
+++ common/config.js
@@ -104,7 +104,7 @@ config.FOOD_GET_VALUE              = function FoodGetValue( r ) {
 //                         VALIDATION SETTINGS                        //
 ////////////////////////////////////////////////////////////////////////
 config.ENABLE_VALIDATION              = true;   // enable validation checks on the server to prevent cheating
-config.FOOD_CAPTURE_EXTRA_TOLERANCE   = 20;     // extra distance that we'll tolerate for valid food capture
+config.FOOD_CAPTURE_EXTRA_TOLERANCE   = 100;     // extra distance that we'll tolerate for valid food capture
 config.PLAYER_CAPTURE_EXTRA_TOLERANCE = 9;      // extra distance that we'll tolerate for valid player capture
 config.SPEED_EXTRA_TOLERANCE          = 0.1;    // extra speed tolerance for movement validation
 config.PLAYER_SCALE_EXTRA_TOLERANCE   = 0.1;    // extra tolerance for player scale
