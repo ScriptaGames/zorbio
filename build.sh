d150a752b9d2c9601da92564d00b086079cad3ee
mwcz <mwc@clayto.com>
2016-02-13 10:07:25 -0500

    include client/sfx in build, #24 #147

--- build.sh
+++ build.sh
@@ -24,6 +24,7 @@ cd ../
 echo "Inlining and minifying content"
 mkdir -p dist > /dev/null
 cp -r client/textures dist/
+cp -r client/sfx dist/
 cp client/favicon.ico dist/
 node node_modules/html-inline/bin/cmd.js -i client/index.html > dist/index-inlined.html
 node node_modules/html-minifier/cli.js dist/index-inlined.html --minify-js --collapse-whitespace --remove-comments  --remove-attribute-quotes --remove-script-type-attributes --remove-style-link-type-attributes > dist/index.html
