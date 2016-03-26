57d7050b1fae7d5d2700e04196d9ae51901c6f0e
Jared Sprague <jared@caramelcode.com>
2016-03-25 21:28:17 -0400

    Added API, fixed bug with throttle on client

--- server/AppServer.js
+++ server/AppServer.js
@@ -1,6 +1,7 @@
 var NODEJS = typeof module !== 'undefined' && module.exports;
 
 var URL        = require('url');
+var basicAuth  = require('basic-auth');
 var config     = require('../common/config.js');
 var pjson      = require('../package.json');
 var request    = require('request');
@@ -15,9 +16,10 @@ var WebSocket  = require('ws');
 /**
  * This module contains all of the app logic and state,
  * @param wss
+ * @param app
  * @constructor
  */
-var AppServer = function (wss) {
+var AppServer = function (wss, app) {
     //  Scope
     var self = this;
 
@@ -29,6 +31,8 @@ var AppServer = function (wss) {
             }
         });
     };
+
+    self.app = app; // express
 
     // Game state
     self.model = new Zorbio.Model(config.WORLD_SIZE, config.FOOD_DENSITY);
@@ -660,6 +664,81 @@ var AppServer = function (wss) {
     if (config.CHECK_VERSION) {
         gameloop.setGameLoop(self.versionCheck, config.CHECK_VERSION_INTERVAL);
     }
+
+
+
+    ///////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////
+
+    // Basic Auth
+    self.basicAuth = function appBasicAuth (req, res, next) {
+        var user = basicAuth(req);
+        if (!user || !user.name || !user.pass) {
+            res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+            res.sendStatus(401);
+            return;
+        }
+
+        if (user.name === 'zoruser' && user.pass === 'Z0r-b!0') {
+            next();
+        } else {
+            res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+            res.sendStatus(401);
+        }
+    };
+    self.app.all("/api/*", self.basicAuth);
+
+    /**
+     * API to return the current count of players on this server
+     */
+    self.app.get('/api/players/count', function (req, res) {
+        var playerIds = Object.getOwnPropertyNames(self.model.players);
+        var count = typeof playerIds.length !== 'undefined' ? playerIds.length : 0;
+        res.setHeader('content-type', 'application/json');
+        res.send( "{\"count\": " + count + "}" );
+    });
+
+    /**
+     * API to return all the player objects on this server
+     */
+    self.app.get('/api/players', function (req, res) {
+        res.setHeader('content-type', 'application/json');
+        res.send( JSON.stringify(self.model.players) );
+    });
+
+    /**
+     * API to return all the actor objects on this server
+     */
+    self.app.get('/api/actors', function (req, res) {
+        res.setHeader('content-type', 'application/json');
+        res.send( JSON.stringify(self.model.actors) );
+    });
+
+    /**
+     * API to return all the actor objects on this server
+     */
+    self.app.get('/api/food', function (req, res) {
+        var foodModel = {};
+        foodModel.foodDensity = self.model.foodDensity;
+        foodModel.foodCount = self.model.foodCount;
+        foodModel.food_respawning_indexes = self.model.food_respawning_indexes;
+        foodModel.food_respawn_ready_queue = self.model.food_respawn_ready_queue;
+
+        res.setHeader('content-type', 'application/json');
+        res.send( JSON.stringify(foodModel) );
+    });
+
+    /**
+     * API to number of socket connections
+     */
+    self.app.get('/api/sockets/count', function (req, res) {
+        var socketIds = Object.getOwnPropertyNames(self.sockets);
+        var count = typeof socketIds.length !== 'undefined' ? socketIds.length : 0;
+        res.setHeader('content-type', 'application/json');
+        res.send( "{\"count\": " + count + "}" );
+    });
+
 };
 
 if (NODEJS) module.exports = AppServer;
