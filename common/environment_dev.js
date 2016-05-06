5b951315b157bb19e290412cd050ab40c67e743a
Jared Sprague <jared@caramelcode.com>
2016-05-06 12:40:12 -0400

    added better dev defaults

--- common/environment_dev.js
+++ common/environment_dev.js
@@ -3,6 +3,9 @@ var NODEJS = typeof module !== 'undefined' && module.exports;
 var ZOR = ZOR || {};
 
 ZOR.Env = {
+    WORLD_SIZE        : 400,
+    FOOD_DENSITY      : 5,
+    FOOD_VALUE        : 10,
     PORT              : 3000,
     DEBUG             : true,
     HEARTBEAT_ENABLE  : false,
@@ -10,6 +13,7 @@ ZOR.Env = {
     ORIGIN            : "http://localhost:3000",
     CHECK_VERSION     : false,
     REQUIRE_ALPHA_KEY : false,
+    AUTO_RUN_ENABLED  : false,
 };
 
 if (NODEJS) module.exports = ZOR.Env;
