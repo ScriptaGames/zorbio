fd727702282d53871e3301bc35554e6c5b393054
Jared Sprague <jared@caramelcode.com>
2016-08-11 22:32:52 -0400

    reset dev defaults

--- common/environment_dev.js
+++ common/environment_dev.js
@@ -3,11 +3,11 @@ var NODEJS = typeof module !== 'undefined' && module.exports;
 var ZOR = ZOR || {};
 
 ZOR.Env = {
-    WORLD_SIZE        : 2000,
-    MAX_BOTS          : 20,
-    FOOD_DENSITY      : 25,
+    WORLD_SIZE        : 1000,
+    MAX_BOTS          : 3,
+    FOOD_DENSITY      : 9,
     INITIAL_PLAYER_RADIUS: 5,
-    FOOD_VALUE        : 0.5,
+    FOOD_VALUE        : 5,
     PORT              : 3000,
     DEBUG             : true,
     HEARTBEAT_ENABLE  : false,
