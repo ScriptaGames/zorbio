8af743b41da62494318e0affa464090add30a793
mwcz <mwc@clayto.com>
2016-09-11 16:53:07 -0400

    don't send Sentry errors in DEBUG mode

--- client/js/Sentry.js
+++ client/js/Sentry.js
@@ -1,1 +1,3 @@
-Raven.config('https://5b9c66b633364e43bc97ec763be5d018@app.getsentry.com/94111').install()
+if (!config.DEBUG) {
+    Raven.config('https://5b9c66b633364e43bc97ec763be5d018@app.getsentry.com/94111').install()
+}
