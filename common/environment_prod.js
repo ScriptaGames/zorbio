02c9ec7a6ce7881eea1373daa93076a1f6989b24
Jared Sprague <jared@caramelcode.com>
2016-09-18 21:27:58 -0400

    removed un-needed config varaible

--- common/environment_prod.js
+++ common/environment_prod.js
@@ -3,7 +3,6 @@ var NODEJS = typeof module !== 'undefined' && module.exports;
 var ZOR = ZOR || {};
 
 ZOR.Env = {
-    ORIGIN            : "http://zor.bio",
     CHECK_VERSION     : true,
     REQUIRE_ALPHA_KEY : true,
 };
