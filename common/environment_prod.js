54d40c27d3561b554467fb5c7fa7c73c1df21f8f
Jared Sprague <jared@caramelcode.com>
2016-10-26 15:51:40 -0400

    temp turn off origin remove alpha key!

--- common/environment_prod.js
+++ common/environment_prod.js
@@ -3,9 +3,9 @@ var NODEJS = typeof module !== 'undefined' && module.exports;
 var ZOR = ZOR || {};
 
 ZOR.Env = {
-    CHECK_ORIGIN      : true,
+    CHECK_ORIGIN      : false,
     CHECK_VERSION     : true,
-    REQUIRE_ALPHA_KEY : true,
+    REQUIRE_ALPHA_KEY : false,
 };
 
 if (NODEJS) module.exports = ZOR.Env;
