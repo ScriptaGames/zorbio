6901de47674280f4565f34869149d3c01f0402bc
Jared Sprague <jared@caramelcode.com>
2016-11-20 13:16:41 -0500

    Fixes #476 close socket when client is connected when they shouldn't be

--- server/AppServer.js
+++ server/AppServer.js
@@ -377,6 +377,7 @@ var AppServer = function (id, app, server_label, port) {
                         if (player_not_in_model_count > config.MAX_NOT_IN_MODEL_ERRORS) {
                             self.log("Player had to many not-in-model errors", player_not_in_model_count);
                             self.kickPlayer(currentPlayer.id, "Disconnected from server");
+                            self.removePlayerSocket(socket_uuid);
                         }
                         self.log("Recieved 'player_update' from player not in model!", sphere.id);
                         break;
