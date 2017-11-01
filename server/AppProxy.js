1f8f66d4a5ecbff7edb9fbf5f5a0a292f7cb61ef
Jared Sprague <jared@caramelcode.com>
2017-11-01 18:11:24 -0400

    code reformat

--- server/AppProxy.js
+++ server/AppProxy.js
@@ -1,9 +1,9 @@
-let config    = require('../common/config.js');
-let ZorApi    = require('./ZorApi.js');
-let AppServer = require('./AppServer.js');
-let pjson     = require('../package.json');
-let rq        = require('request');
-let cookie    = require('cookie');
+let config    = require( '../common/config.js' );
+let ZorApi    = require( './ZorApi.js' );
+let AppServer = require( './AppServer.js' );
+let pjson     = require( '../package.json' );
+let rq        = require( 'request' );
+let cookie    = require( 'cookie' );
 
 
 let AppProxy = function(wss, app, server_label, port) {
@@ -15,19 +15,19 @@ let AppProxy = function(wss, app, server_label, port) {
     self.gameInstances = [];
 
     for (let i = 0; i < config.NUM_GAME_INSTANCES; i++) {
-        self.gameInstances.push(new AppServer(i, self.app, server_label, port));
+        self.gameInstances.push( new AppServer( i, self.app, server_label, port ) );
     }
 
     // set up api
-    self.api = new ZorApi(self.app, self.gameInstances);
+    self.api = new ZorApi( self.app, self.gameInstances );
 
     // Route to write sticky session cookie for linode nodebalancer at given location
-    self.app.get('/nbsrv/:nb/:id', function(req, res) {
+    self.app.get( '/nbsrv/:nb/:id', function(req, res) {
         let balancer = req.params.nb;
-        let node_id = req.params.id;
+        let node_id  = req.params.id;
 
         // Set a new cookie with the name
-        self.setSrvIdCookie(node_id, res);
+        self.setSrvIdCookie( node_id, res );
 
         // Get the subdomain of the balancer location
         let balancer_domain = config.BALANCERS[balancer];
@@ -35,27 +35,27 @@ let AppProxy = function(wss, app, server_label, port) {
         // bounce redirect to balancer subdomain to set the cookie there too
         res.statusCode = 302;
         let bounce_url = 'http://' + balancer_domain + '/nbsrv_bounce/' + node_id;
-        res.setHeader('Location', bounce_url);
+        res.setHeader( 'Location', bounce_url );
         res.end();
-    });
+    } );
 
     // Route to set the cookie at the nodebalancer subdomain and bounce back to homepage
-    self.app.get('/nbsrv_bounce/:id', function(req, res) {
+    self.app.get( '/nbsrv_bounce/:id', function(req, res) {
         let node_id = req.params.id;
 
         // Set a new cookie with the name
-        self.setSrvIdCookie(node_id, res);
+        self.setSrvIdCookie( node_id, res );
 
         // bounce back to homepage
         res.statusCode = 302;
-        res.setHeader('Location', 'http://zor.bio/');
+        res.setHeader( 'Location', 'http://zor.bio/' );
         res.end();
-    });
+    } );
 
-    self.wss.on('connection', function wssConnection(ws, req) {
+    self.wss.on( 'connection', function wssConnection(ws, req) {
         for (let i = 0; i < self.gameInstances.length; i++) {
             if (!self.gameInstances[i].isFull()) {
-                self.gameInstances[i].addClient(ws, req);
+                self.gameInstances[i].addClient( ws, req );
                 break;
             }
 
@@ -63,13 +63,13 @@ let AppProxy = function(wss, app, server_label, port) {
                 // all game instances full
                 // Assign any other connections to lowest population server
 
-                console.warn('All game instances full!');
+                console.warn( 'All game instances full!' );
 
                 let low_pop_index = self.findLowestPopInstanceIndex();
-                self.gameInstances[low_pop_index].addClient(ws, req);
+                self.gameInstances[low_pop_index].addClient( ws, req );
             }
         }
-    });
+    } );
 
     /**
      * Returns the index of the lowest population game instance
@@ -77,11 +77,11 @@ let AppProxy = function(wss, app, server_label, port) {
      */
     self.findLowestPopInstanceIndex = function proxyFindLowestPopInstance() {
         let lowest_pop_index = 0;
-        let client_count = Infinity;
+        let client_count     = Infinity;
         for (let i = 0; i < self.gameInstances.length; i++) {
             let cur_client_count = self.gameInstances[i].getClientCount();
             if (cur_client_count < client_count) {
-                client_count = cur_client_count;
+                client_count     = cur_client_count;
                 lowest_pop_index = i;
             }
         }
@@ -94,49 +94,49 @@ let AppProxy = function(wss, app, server_label, port) {
             url: 'https://raw.githubusercontent.com/ScriptaGames/zorbio-version/master/version.json',
         };
 
-        console.log('Checking version...');
+        console.log( 'Checking version...' );
 
-        rq.get(options, function(error, response, body) {
+        rq.get( options, function(error, response, body) {
             if (!error && response.statusCode === 200) {
                 try {
-                    let res = JSON.parse(body);
+                    let res           = JSON.parse( body );
                     let local_version = pjson.version + '-' + pjson.build;
 
                     if (local_version !== res.version) {
-                        console.log('version out of date, local  version:', local_version);
-                        console.log('version out of date, remote version:', res.version);
+                        console.log( 'version out of date, local  version:', local_version );
+                        console.log( 'version out of date, remote version:', res.version );
 
                         // notify all game instances that the version is out of date
                         for (let i = 0; i < self.gameInstances.length; i++) {
-                            self.gameInstances[i].setServerMessage('Server restart imminent!');
+                            self.gameInstances[i].setServerMessage( 'Server restart imminent!' );
                         }
                     }
                     else {
-                        console.log('Version is up-to-date', local_version);
+                        console.log( 'Version is up-to-date', local_version );
                     }
                 }
                 catch (e) {
-                    console.error('Caught exception parsing json from zorbio-version');
+                    console.error( 'Caught exception parsing json from zorbio-version' );
                 }
             }
             else {
-                console.error('Error response code form getting zorbio-version from github');
+                console.error( 'Error response code form getting zorbio-version from github' );
             }
-        }).on('error', function() {
-            console.error('Error http request failed to get zorbio-version from github');
-        }).end();
+        } ).on( 'error', function() {
+            console.error( 'Error http request failed to get zorbio-version from github' );
+        } ).end();
     };
 
     self.setSrvIdCookie = function appSetSrvIdCookie(id, res) {
         if (id) {
-            res.setHeader('Set-Cookie', cookie.serialize('NB_SRVID', id, {
+            res.setHeader( 'Set-Cookie', cookie.serialize( 'NB_SRVID', id, {
                 path: '/',
-            }));
+            } ) );
         }
     };
 
     if (config.CHECK_VERSION) {
-        setInterval(self.versionCheck, config.CHECK_VERSION_INTERVAL);
+        setInterval( self.versionCheck, config.CHECK_VERSION_INTERVAL );
     }
 };
 
