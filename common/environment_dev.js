8109cc6282340023a4a49a5dc3f4f32797deab39
Jared Sprague <jared@caramelcode.com>
2017-11-15 22:57:41 -0500

    balance which skins bots will choose #488

--- common/environment_dev.js
+++ common/environment_dev.js
@@ -7,7 +7,7 @@ const NODEJS_ENV_DEV = typeof module !== 'undefined' && module.exports;
 
 const DEV_SETTINGS = {
     WORLD_SIZE            : 1000,
-    MAX_BOTS              : 5,
+    MAX_BOTS              : 10,
     FOOD_DENSITY          : 5,
     INITIAL_PLAYER_RADIUS : 5,
     FOOD_VALUE            : 5,
@@ -17,11 +17,11 @@ const DEV_SETTINGS = {
     CHECK_ORIGIN          : false,
     CHECK_VERSION         : false,
     REQUIRE_ALPHA_KEY     : false,
-    AUTO_RUN_ENABLED      : !true,
+    AUTO_RUN_ENABLED      : true,
     NUM_GAME_INSTANCES    : 1,
     ENABLE_HTTP_SERVER    : true,
     ENABLE_BACKEND_SERVICE: false,
-    BOT_DEFAULT_MOVEMENT  : 'hold',
+    BOT_DEFAULT_MOVEMENT  : 'curve',
 };
 
 if (NODEJS_ENV_DEV) {
