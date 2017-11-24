62e606176b2878e41ad8a5de29994ff60a981280
Michael Clayton <mclayton@redhat.com>
2017-11-23 22:49:27 -0500

    stop playing woosh sound on boot, fixes #423

--- client/js/Sounds.js
+++ client/js/Sounds.js
@@ -156,10 +156,6 @@ ZOR.Sounds = (function ZORSounds() {
         sound.mute(true, id);
         sound.volume(0, id);
         sound.pos(1, 1, 1, id);
-
-        // woosh sound from wad needs to be started and stopped
-        sounds.sfx.woosh.play();
-        sounds.sfx.woosh.stop();
     }
     initSfxHacks();
 
