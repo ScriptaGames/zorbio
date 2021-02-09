737bb95ce2b30e8d815bac2856b4715601a7b037
Jared Sprague <jared@caramelcode.com>
2021-02-08 19:31:34 -0500

    updated build.sh to not build or upload rpms also fixed bug with json parsing

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# I think this dependancy was replaced with util/jsawk
-#if [ ! -f /usr/bin/js ]; then
-#    echo "ERROR: missing dependancy js. 'dnf install js-devel' to fix."
-#    exit
-#fi
-
 #Gather values to replace in the templates
-VERSION="$(util/jsawk 'return this.version' < package.json)"
-BUILD="$(util/jsawk 'return this.build' < package.json)"
-GIT_REF="$(git rev-parse HEAD)"
+VERSION=$(jq '.version' -r < package.json)
+BUILD=$(jq '.build' -r < package.json)
+GIT_REF=$(git rev-parse HEAD)
 
 SCRIPT_DIR=$( dirname $(realpath --relative-base=../ "$0") )
 
@@ -63,76 +57,13 @@ sed -i "s/{{ VERSION }}/$VERSION/g" dist/index.html
 sed -i "s/{{ BUILD }}/$BUILD/g" dist/index.html
 sed -i "s/{{ GIT_REF }}/$GIT_REF/g" dist/index.html
 
-# Inject the google adsense script after build because of inliner conflicts
-GOOGLE_AD_SCRIPT='<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'
-sed -i "s,</head>,$GOOGLE_AD_SCRIPT</head>,g" dist/index.html
-
 # Inject the social sharring buttons
 SHARE_BUTTONS='<script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5a1ef0c73ab78300126056a6&product=sticky-share-buttons"></script>'
 sed -i "s,{{ SHARE_BUTTONS }},$SHARE_BUTTONS,g" dist/index.html
 
 echo "dist/index.html written"
 
-if [ "$1" = "--dist-dev" ]; then
-    exit
-fi
-
-###############################################
-# Generate the RPM spec files
-###############################################
-echo "Generating RPM spec file: zorbio.spec"
-cd build_files
-rm -f *.spec
-cp zorbio.spec.template zorbio.spec
-cp zorbio-static.spec.template zorbio-static.spec
-sed -i "s/VERSION/$VERSION/g" zorbio.spec
-sed -i "s/BUILD/$BUILD/g" zorbio.spec
-sed -i "s/VERSION/$VERSION/g" zorbio-static.spec
-sed -i "s/BUILD/$BUILD/g" zorbio-static.spec
-cp *.spec ~/rpmbuild/SPECS/
-cd ..
-
-
-###############################################
-# Build the rpms
-###############################################
-echo "Building the rpms"
-# save a copy of the current node_modules
-mv node_modules node_modules_orig
-
-# remove current node modules to get rid of dev dependancies
-rm -rf node_modules
-
-# Install only production dependancies
-echo "Install production dependancies"
-npm install --production
-
-# Tar up the source for the rpm build
-echo "tar source"
-tar -czf ~/rpmbuild/SOURCES/zorbio.tar.gz ../$SCRIPT_DIR --transform="s/^$SCRIPT_DIR/zorbio/"
-
-# Restore original node_modules
-echo "clean up node modules"
-rm -rf node_modules
-mv node_modules_orig node_modules
-
 # restore orig environment.js symlink back to dev
 cd common/
 ln -f -s ./environment_dev.js ./environment.js
 
-# Finally build the rpm
-cd ~/rpmbuild
-echo "Building the server rpm"
-rpmbuild -bb SPECS/zorbio.spec
-echo "Building the static rpm"
-rpmbuild -bb SPECS/zorbio-static.spec
-
-###############################################
-# Upload the rpm and update yum repo database
-###############################################
-if [ "$1" = "--upload" ]; then
-    echo "Uploading rpms..."
-    scp -P 4460 ~/rpmbuild/RPMS/x86_64/zorbio-$VERSION-$BUILD.fc26.x86_64.rpm mcp.zorb.io:/var/www/html/repo/
-    scp -P 4460 ~/rpmbuild/RPMS/x86_64/zorbio-static-$VERSION-$BUILD.fc26.x86_64.rpm mcp.zorb.io:/var/www/html/repo/
-    echo "Done. Repo will be updated within 5 minutes. Edit: https://github.com/ScriptaGames/zorbio-version/edit/master/version.json"
-fi
